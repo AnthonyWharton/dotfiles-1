f296f6d4de27ebf4b74426eabe21a0d5470e0979
Raef Coles <RaefColes@gmail.com>
2017-01-29 16:24:07 +0000

    initial commit

---
+++ monitors/.scripts/startup/screenlayout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --mode 1280x1024 --pos 4920x584 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 0x128 --rotate left --output DP-5 --off --output DP-4 --primary --mode 3840x2160 --pos 1080x0 --rotate normal --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --off
